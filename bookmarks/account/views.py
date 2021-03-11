106367f32fb242b8bd46fc0d5ba02aa93995ddbc
Ayush Nanda <ananda_be18@thapar.edu>
2021-03-11 16:53:28 +0530

    user registration error resolved #working

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,27 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import UserRegistrationForm
 # Create your views here.
 
 
-def user_login(request):
+def register(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user .is_active:
-                    login(request,user)
-                    return HttpResponse('Authneticated Successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid Login')
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+        #Create a new user object but avoid saving it yet    
+            new_user = user_form.save(commit=False)
+            #Set the choosen password:
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            #Save the user object
+            new_user.save()
+            return render(request, 'account/register_done.html' , {'new_user':new_user})
     else:
-        form = LoginForm()
-    return render(request, 'account/login.html' , {'form':form})
+        user_form=UserRegistrationForm()
+    return render(request,'account/register.html',{'user_form': user_form})
+    
 
 from	django.contrib.auth.decorators	import	login_required
 @login_required
